ba6ebef14c077a82066b983e4127ca2dbd101032
fork0800092000 <fork21342@gmail.com>
2019-10-18 19:07:20 +0800

    Add files via upload

---
+++ hw2Pgm.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 18 14:28:22 2019
+
+@author: aaa
+"""
+
+import numpy as np
+import pandas as pd
+import csv
+
+dat = pd.read_csv('train.csv',encoding = 'cp950')
