92ee837e48b014852d4ba0eb3bf295d711ec9725
fork0800092000 <fork21342@gmail.com>
2019-10-23 13:00:42 +0800

    Add files via upload

--- MLP.py
+++ MLP.py
@@ -1,21 +1,24 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Oct 21 20:05:20 2019
+Created on Tue Oct 22 23:28:35 2019
 
-@author: aaa
+@author: chenxuezhi
 """
+
 
 import numpy as np
 import matplotlib.pyplot as plt
 from random import random
 from random import seed
+from math import exp
 
 
 data = np.genfromtxt('perceptron1.txt',delimiter = '')
 train = data[:,:2]
 label = data[:,2]
 train_c = np.concatenate((-(np.ones(shape = (train.shape[0],1))),train),axis = 1).astype(float)
-train_set = train_c[:int(2/3*train_c.shape[0]),:]
+train_set = np.array(train_c[:int(2/3*train_c.shape[0]),:])
 train_label = label[:int(2/3*label.shape[0]),]
 test_set = train_c[int(2/3*train_c.shape[0]):,:]
 
@@ -33,9 +36,33 @@ network = initialize_network(2,2,1)
 for layer in network:
     print(layer)
     
-def activation(weights, inputs):
+def activation(neurons, inputs):
     sum = 0
-    sum += np.dot(weights,inputs.T)
+    X = np.array(inputs)
+    weight = np.array(neurons['weights'])
+    sum += np.dot(weight,X.T)
     return sum
+
 def sigmoid(activation):
     return 1.0 / (1.0 + exp(-activation))
+
+def forward_propagation(network,inputs):
+    inputss = inputs
+    for layer in network:
+        new_inputs = []
+        for neuron in layer:
+            activate = activation(neuron, inputss)
+            neuron['output'] = sigmoid(activate)
+            new_inputs.append(neuron['output'])
+        inputs = new_inputs
+    return inputs
+
+Output = []
+for i in range(train_set.shape[0]):
+    Output.append(forward_propagation(network,train_set[i]))
+print(Output)
+            
+            
+            
+            
+
